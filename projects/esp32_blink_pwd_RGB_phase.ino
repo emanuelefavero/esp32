f0f87113ce52287e4bb018e6261c537e9437a95d
emanuelefavero <emafaux89@gmail.com>
2024-05-13 20:37:49 +0200

    add projects/blink_pwd_RGB_phase

---
+++ projects/esp32_blink_pwd_RGB_phase.ino
@@ -0,0 +1,67 @@
+#define LEDC_TIMER_13_BIT 13
+#define LEDC_BASE_FREQ 5000
+
+#define LEDC_CHANNEL_0 0 // Red channel
+#define LEDC_CHANNEL_1 1 // Green channel
+#define LEDC_CHANNEL_2 2 // Blue channel
+
+#define RED_PIN 2 // Red on Pin 2
+#define GREEN_PIN 4 // Green on Pin 4
+#define BLUE_PIN 5 // Blue on Pin 5
+
+int brightnessRedMax = 240;
+int brightnessGreenMax = 200;
+int brightnessBlueMax = 255;
+
+// TIP: We assign different variables and set different values to each led to offset the pwd phase of each led
+int brightnessRed = 80;
+int brightnessGreen = 30;
+int brightnessBlue = 180;
+
+// TIP: Increase the value of fadeAmount to increase the pwd speed
+int fadeAmountRed = 1;
+int fadeAmountGreen = 3;
+int fadeAmountBlue = 4;
+
+void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
+  uint32_t duty = (8191 / valueMax) * min(value, valueMax);
+  ledcWrite(channel, duty);
+}
+
+void setup() {
+  // Setup each channel
+  ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
+  ledcSetup(LEDC_CHANNEL_1, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
+  ledcSetup(LEDC_CHANNEL_2, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
+
+  // Attach each channel
+  ledcAttachPin(RED_PIN, LEDC_CHANNEL_0);
+  ledcAttachPin(GREEN_PIN, LEDC_CHANNEL_1);
+  ledcAttachPin(BLUE_PIN, LEDC_CHANNEL_2);
+}
+
+void loop() {
+  // Apply PWD to each channel
+  ledcAnalogWrite(LEDC_CHANNEL_0, brightnessRed, brightnessRedMax); // Red
+  ledcAnalogWrite(LEDC_CHANNEL_1, brightnessGreen, brightnessGreenMax); // Green
+  ledcAnalogWrite(LEDC_CHANNEL_2, brightnessBlue, brightnessBlueMax); // Blue
+
+  // Change brightness over time
+  brightnessRed += fadeAmountRed;
+  if (brightnessRed <= 0 || brightnessRed >= brightnessRedMax) {
+    fadeAmountRed = -fadeAmountRed; // Reverse the direction for Red
+  }
+
+  brightnessGreen += fadeAmountGreen;
+  if (brightnessGreen <= 0 || brightnessGreen >= brightnessGreenMax) {
+    fadeAmountGreen = -fadeAmountGreen; // Reverse the direction for Green
+  }
+
+  brightnessBlue += fadeAmountBlue;
+  if (brightnessBlue <= 0 || brightnessBlue >= brightnessBlueMax) {
+    fadeAmountBlue = -fadeAmountBlue; // Reverse the direction for Blue
+  }
+
+  // PWD Speed
+  delay(125);
+}
