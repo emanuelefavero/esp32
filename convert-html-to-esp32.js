4190c2f7a8753844b0f06a5a8d06e021f4ca4c45
emanuelefavero <emafaux89@gmail.com>
2024-05-14 17:13:46 +0200

    add convert-html-to-esp32

---
+++ convert-html-to-esp32.js
@@ -0,0 +1,19 @@
+/*
+You can use this file to convert an HTML file to a format that can be used in an ESP32 project.
+// TIP: Edit the `index.html` file to include the HTML code you want to convert, then run this file with Node.js or Nodemon: `node convert-html-to-esp32.js` or `nodemon convert-html-to-esp32.js`
+*/
+
+const fs = require('fs')
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: fs.createReadStream('index.html'),
+  output: process.stdout,
+  terminal: false,
+})
+
+rl.on('line', (line) => {
+  console.log(`client.println("${line}")`)
+})
+
+rl.on('close', () => console.log('\nFinished reading file'))
