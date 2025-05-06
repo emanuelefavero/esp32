40fa7ce33a9a281bb6edafff2f6969f46435d955
Emanuele Favero <emafaux89@gmail.com>
2025-05-06 04:38:44 +0200

    refactor esp32_tetris_ttgo

--- projects/esp32_tetris_ttgo/esp32_tetris_ttgo.ino
+++ projects/esp32_tetris_ttgo/esp32_tetris_ttgo.ino
@@ -1,9 +1,9 @@
-//========================================================================
-// TETRIS with M5STACK : 2018.01.20 Transplant by macsbug
+/*
+// TETRIS for Lilygo TTGO (ESP32)
 
-// Github     : https://macsbug.wordpress.com/2018/01/20/tetris-with-m5stack/
-//========================================================================
-
+// Forked Github Repo (modified by emanuelefavero): 
+@see https://macsbug.wordpress.com/2018/01/20/tetris-with-m5stack/
+*/
 
 #include <SPI.h>
 #include <TFT_eSPI.h>
@@ -60,17 +60,16 @@ unsigned long inactivityStartTime = 0;
 bool inactivityTimerRunning = false;
 
 void setup(void) {
-   
   pinMode(37,INPUT_PULLUP);
   
   pinMode(leftButton,INPUT_PULLUP);
   pinMode(rightButton,INPUT_PULLUP);
-   tft.init();
+  tft.init();
   tft.setRotation(0);
   tft.setSwapBytes(true);
 
-   tft.pushImage(0, 0, 135, 240,tet);
-   delay(3000);
+  tft.pushImage(0, 0, 135, 240,tet);
+  delay(3000);
   tft.fillScreen(TFT_BLACK);  
   tft.drawLine(11,19,122,19,GREY);
 
@@ -79,27 +78,26 @@ void setup(void) {
   
   tft.drawString("SCORE:"+String(score),14,8,1);
   tft.drawString("LVL:"+String(lvl),88,8,1);
-  //----------------------------// Make Block ----------------------------
-  make_block( 0, TFT_BLACK);        // Type No, Color
-  make_block( 1, 0x00F0);       // DDDD     RED
-  make_block( 2, 0xFBE4);       // DD,DD    PUPLE 
-  make_block( 3, 0xFF00);       // D__,DDD  BLUE
-  make_block( 4, 0xFF87);       // DD_,_DD  GREEN 
-  make_block( 5, 0x87FF);       // __D,DDD  YELLO
-  make_block( 6, 0xF00F);       // _DD,DD_  LIGHT GREEN
-  make_block( 7, 0xF8FC);       // _D_,DDD  PINK
-  //----------------------------------------------------------------------
 
+  // Make Block
+  makeBlock( 0, TFT_BLACK);    // Type No, Color
+  makeBlock( 1, 0x00F0);       // DDDD     RED
+  makeBlock( 2, 0xFBE4);       // DD,DD    PUPLE 
+  makeBlock( 3, 0xFF00);       // D__,DDD  BLUE
+  makeBlock( 4, 0xFF87);       // DD_,_DD  GREEN 
+  makeBlock( 5, 0x87FF);       // __D,DDD  YELLO
+  makeBlock( 6, 0xF00F);       // _DD,DD_  LIGHT GREEN
+  makeBlock( 7, 0xF8FC);       // _D_,DDD  PINK
 
-  PutStartPos();                             // Start Position
+  putStartPos();                             // Start Position
   for (int i = 0; i < 4; ++i) screen[pos.X + 
-   block.square[rot][i].X][pos.Y + block.square[rot][i].Y] = block.color;
-  Draw();                                    // Draw block
+  block.square[rot][i].X][pos.Y + block.square[rot][i].Y] = block.color;
+  draw();                                    // draw block
 }
-//========================================================================
+
 void loop() {
   if (gameover || !started) {
-    CheckInactivity(); // Check for inactivity to enter sleep mode
+    checkInactivity(); // Check for inactivity to enter sleep mode
 
     if (digitalRead(leftButton) == 0) {
       for (int j = 0; j < Height; ++j)
@@ -112,13 +110,13 @@ void loop() {
       game_speed = 20;
       lvl = 1;
 
-      PutStartPos();  // Start Position
+      putStartPos();  // Start Position
       for (int i = 0; i < 4; ++i)
         screen[pos.X + block.square[rot][i].X][pos.Y + block.square[rot][i].Y] = block.color;
 
       tft.drawString("SCORE:" + String(score), 14, 8, 1);
       tft.drawString("LVL:" + String(lvl), 88, 8, 1);
-      Draw();
+      draw();
 
       // Reset inactivity timer on game start
       inactivityTimerRunning = false;
@@ -133,27 +131,28 @@ void loop() {
   if (gameover == false) {
     Point next_pos;
     int next_rot = rot;
-    GetNextPosRot(&next_pos, &next_rot);
-    ReviseScreen(next_pos, next_rot);
+    getNextPosRot(&next_pos, &next_rot);
+    reviseScreen(next_pos, next_rot);
     delay(game_speed);  // SPEED ADJUST
   }
 }
+
 //========================================================================
-void Draw() {                               // Draw 120x240 in the center
+void draw() {                               // draw 120x240 in the center
   for (int i = 0; i < Width; ++i) for (int j = 0; j < Height; ++j)
-   for (int k = 0; k < Length; ++k) for (int l = 0; l < Length; ++l)
+  for (int k = 0; k < Length; ++k) for (int l = 0; l < Length; ++l)
     backBuffer[j * Length + l][i * Length + k] = BlockImage[screen[i][j]][k][l];
     tft.pushImage(12, 20, 110, 220,*backBuffer);
 }
-//========================================================================
-void PutStartPos() {
+
+void putStartPos() {
   game_speed=20;
   pos.X = 4; pos.Y = 1;
   block = blocks[random(7)];
   rot = random(block.numRotate);
 }
-//========================================================================
-bool GetSquares(Block block, Point pos, int rot, Point* squares) {
+
+bool getSquares(Block block, Point pos, int rot, Point* squares) {
   bool overlap = false;
   for (int i = 0; i < 4; ++i) {
     Point p;
@@ -165,49 +164,46 @@ bool GetSquares(Block block, Point pos, int rot, Point* squares) {
   }
   return !overlap;
 }
-//========================================================================
-void GameOver() {
+
+void SetGameOver() {
   for (int i = 0; i < Width; ++i) for (int j = 0; j < Height; ++j)
   if (screen[i][j] != 0) screen[i][j] = 4;
   gameover = true;
  
 }
-//========================================================================
-void ClearKeys() { but_A=false; but_LEFT=false; but_RIGHT=false;}
-//========================================================================
 
-bool KeyPadLoop(){
+void clearKeys() { but_A=false; but_LEFT=false; but_RIGHT=false;}
+
+bool keyPadLoop(){
   if(digitalRead(leftButton)==0 && digitalRead(rightButton)==1){
     if(pom==0)
   {
     pom=1;
-    ClearKeys();
+    clearKeys();
     but_LEFT =true;
     return true;}
     }else {pom=0;}
     
   if(digitalRead(rightButton)==0 && digitalRead(leftButton)==1){
   if(pom2==0)
-  {pom2=1;ClearKeys();but_RIGHT=true;return true;}
-  }else {pom2=0;}
+  {pom2=1;clearKeys();but_RIGHT=true;return true;}
+  } else {pom2=0;}
 
- 
- 
   if(digitalRead(37)==0){
     if(pom3==0)
-  {pom3=1;ClearKeys();but_A    =true;return true;}
+  {pom3=1;clearKeys();but_A    =true;return true;}
   }else {pom3=0;}
 
   if(digitalRead(rightButton)==0 && digitalRead(leftButton)==0){
     if(pom4==0)
-  {pom4=1;ClearKeys();but_A =true;return true;}
+  {pom4=1;clearKeys();but_A =true;return true;}
   }else {pom4=0;}
   
   return false;
 }
-//========================================================================
-void GetNextPosRot(Point* pnext_pos, int* pnext_rot) {
-  bool received = KeyPadLoop();
+
+void getNextPosRot(Point* pnext_pos, int* pnext_rot) {
+  bool received = keyPadLoop();
 
   if (but_LEFT) started = true;
   if (!started) return;
@@ -222,66 +218,70 @@ void GetNextPosRot(Point* pnext_pos, int* pnext_rot) {
     }
   }
 }
-//========================================================================
-void DeleteLine() {
+
+void deleteLine() {
   for (int j = 0; j < Height; ++j) {
     bool Delete = true;
     for (int i = 0; i < Width; ++i) if (screen[i][j] == 0) Delete = false;
-    if (Delete)
-    {
-       score++;
-       if(score%5==0)
-        {
+    if (Delete) {
+      score++;
+      if(score%5==0) {
         lvl++;
         game_speed=game_speed-4;
         tft.drawString("LVL:"+String(lvl),88,8,1);
-        }
-       tft.drawString("SCORE:"+String(score),14,8,1);
-          for (int k = j; k >= 1; --k) 
-              {
+      }
       
-    
-    for (int i = 0; i < Width; ++i)
-    {
-      screen[i][k] = screen[i][k - 1];
+      tft.drawString("SCORE:"+String(score),14,8,1);
+
+      for (int k = j; k >= 1; --k) {
+        for (int i = 0; i < Width; ++i) {
+          screen[i][k] = screen[i][k - 1];
+        }
+      }
     }
   }
-}}}
-//========================================================================
-void ReviseScreen(Point next_pos, int next_rot) {
+}
+
+void reviseScreen(Point next_pos, int next_rot) {
   if (!started) return;
+
   Point next_squares[4];
+
   for (int i = 0; i < 4; ++i) screen[pos.X + 
     block.square[rot][i].X][pos.Y + block.square[rot][i].Y] = 0;
-  if (GetSquares(block, next_pos, next_rot, next_squares)) {
-   for (int i = 0; i < 4; ++i){
-     screen[next_squares[i].X][next_squares[i].Y] = block.color;
-   }
-   pos = next_pos; rot = next_rot;
-  }
-  else {
-   for (int i = 0; i < 4; ++i) screen[pos.X + 
-    block.square[rot][i].X][pos.Y + block.square[rot][i].Y] = block.color;
-   if (next_pos.Y == pos.Y + 1) {
-    DeleteLine(); PutStartPos();
-    if (!GetSquares(block, pos, rot, next_squares)) {
-     for (int i = 0; i < 4; ++i) screen[pos.X + 
-      block.square[rot][i].X][pos.Y + block.square[rot][i].Y] = block.color;
-      GameOver();
+
+  if (getSquares(block, next_pos, next_rot, next_squares)) {
+    for (int i = 0; i < 4; ++i){
+      screen[next_squares[i].X][next_squares[i].Y] = block.color;
     }
-   }
+
+    pos = next_pos; rot = next_rot;
+  } else {
+    for (int i = 0; i < 4; ++i) screen[pos.X + 
+    block.square[rot][i].X][pos.Y + block.square[rot][i].Y] = block.color;
+
+    if (next_pos.Y == pos.Y + 1) {
+      deleteLine(); putStartPos();
+
+      if (!getSquares(block, pos, rot, next_squares)) {
+        for (int i = 0; i < 4; ++i) screen[pos.X + 
+        block.square[rot][i].X][pos.Y + block.square[rot][i].Y] = block.color;
+        SetGameOver();
+      }
+    }
   }
-  Draw();
+
+  draw();
 }
-//========================================================================
-void make_block( int n , uint16_t color ){            // Make Block color       
+
+void makeBlock( int n , uint16_t color ){            // Make Block color       
   for ( int i =0 ; i < 12; i++ ) for ( int j =0 ; j < 12; j++ ){
     BlockImage[n][i][j] = color;                           // Block color
     if ( i == 0 || j == 0 ) BlockImage[n][i][j] = 0;       // TFT_BLACK Line
   } 
 }
-//========================================================================
-void CheckInactivity() {
+
+void checkInactivity() {
   // Start or update inactivity timer
   if (!inactivityTimerRunning) {
     inactivityStartTime = millis();
